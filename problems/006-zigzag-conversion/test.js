d4ed3d3d75d37810f70dd50ed62b79a9beb4e035
zerlous <lzheng.zj@gmail.com>
2018-10-18 10:24:57 +0800

    add

---
+++ problems/006-zigzag-conversion/test.js
@@ -0,0 +1,15 @@
+/**
+ * @Author: zerlous
+ * @Date: 2018/10/18
+ * @Description:
+ */
+
+module.exports = [
+    {
+        input:['PAYPALISHIRING',3],
+        output:'PAHNAPLSIIGYIR'
+    },{
+        input:['PAYPALISHIRING',4],
+        output:'PINALSIGYAHRPI'
+    }
+];
